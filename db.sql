59f8f6675ad66816a9a2b7368fc299bcafea9990
melanie <mmm82418@lasallebajio.edu.mx>
2025-11-29 21:42:29 -0600

    Agregar archivo SQL de la base de datos db_restaurante

---
+++ db.sql
@@ -0,0 +1,168 @@
+﻿-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_restaurante
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contactos`
+--
+
+DROP TABLE IF EXISTS `contactos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contactos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `telefono` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactos`
+--
+
+LOCK TABLES `contactos` WRITE;
+/*!40000 ALTER TABLE `contactos` DISABLE KEYS */;
+INSERT INTO `contactos` VALUES (1,NULL),(2,NULL),(3,NULL),(4,NULL);
+/*!40000 ALTER TABLE `contactos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fechas_reservacion`
+--
+
+DROP TABLE IF EXISTS `fechas_reservacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fechas_reservacion` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fechas_reservacion`
+--
+
+LOCK TABLES `fechas_reservacion` WRITE;
+/*!40000 ALTER TABLE `fechas_reservacion` DISABLE KEYS */;
+INSERT INTO `fechas_reservacion` VALUES (1,'2025-11-29'),(2,'2025-11-28'),(3,'2025-12-04'),(4,'2025-12-01');
+/*!40000 ALTER TABLE `fechas_reservacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `opiniones`
+--
+
+DROP TABLE IF EXISTS `opiniones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `opiniones` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `calificacion` int NOT NULL,
+  `comentario` text,
+  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `opiniones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `opiniones`
+--
+
+LOCK TABLES `opiniones` WRITE;
+/*!40000 ALTER TABLE `opiniones` DISABLE KEYS */;
+INSERT INTO `opiniones` VALUES (1,2,'mariana',2,'LL','2025-11-29 08:10:39'),(2,5,'mario',5,'un lugar muy bonito y comida excelente ','2025-11-29 23:06:47'),(3,12,'karen',5,'excelente lugar','2025-11-29 23:17:53'),(4,13,'vero',5,'buen ambiente familiar','2025-11-30 00:03:52');
+/*!40000 ALTER TABLE `opiniones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservaciones`
+--
+
+DROP TABLE IF EXISTS `reservaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservaciones` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `personas` int NOT NULL,
+  `mensaje` text,
+  `contacto_id` int DEFAULT NULL,
+  `fecha_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `contacto_id` (`contacto_id`),
+  KEY `fecha_id` (`fecha_id`),
+  CONSTRAINT `reservaciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `reservaciones_ibfk_2` FOREIGN KEY (`contacto_id`) REFERENCES `contactos` (`id`),
+  CONSTRAINT `reservaciones_ibfk_3` FOREIGN KEY (`fecha_id`) REFERENCES `fechas_reservacion` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservaciones`
+--
+
+LOCK TABLES `reservaciones` WRITE;
+/*!40000 ALTER TABLE `reservaciones` DISABLE KEYS */;
+INSERT INTO `reservaciones` VALUES (1,2,'mariana garcia',3,NULL,1,1),(2,5,'mario',5,'somos muchos',2,2),(3,12,'luis',10,'tenemos una persona en silla de ruedas',3,3),(4,13,'vero',3,'es un cumplea├▒os',4,4);
+/*!40000 ALTER TABLE `reservaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `contrase├▒a` varchar(255) NOT NULL,
+  `fecha_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `telefono` (`telefono`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'lili','1234567890','$2b$10$yXTcQSEguyVLvGrOdBzD8uTIXmOMehnDOv1G9NYeDcCS7FUlIcCRG','2025-11-29 05:55:09'),(2,'mariana','4778908765','$2b$10$305i0qhT1ibD3i.afEYbfuJ5mk9jxiG6oWYf8pO.NJrB0pJaK0v4e','2025-11-29 06:46:00'),(5,'mario','0000000000','$2b$10$IVqjKtNEXy29C5FQcRdbI.Xk1FtFYOjGeq1r4fsCON9IEDp.t5IQO','2025-11-29 23:05:28'),(12,'karen','1111111111','$2b$10$Y72m/Xy/5UfnypYUu4CRMu0A8vp7N6z8izqf13PNucTP4V/0AdE5O','2025-11-29 23:15:49'),(13,'vero','5555555555','$2b$10$eqZknpS35cs76z1ZBYrffe5eIZMOk/rkEtVrxeZhLuAo.DUril0pq','2025-11-30 00:02:32');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-29 21:39:49
